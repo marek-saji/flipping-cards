95da36a6c2aab9b0a2321dec6b4b1fc47f6edc98
Marek ‘saji’ Augustynowicz <marek.aug@gmail.com>
2016-03-13 21:48:52 +0100

    Practice mode

---
+++ Card.js
@@ -0,0 +1,316 @@
+(function (global, document, undefined) {
+
+    'use strict';
+
+    
+    /**
+     * Create a new flippin’ card
+     *
+     * @param {Element} container - DOM Element to append card to
+     * @param {object} itemGenerator.next - Used to get next item. Must return object with `value` property.
+     * @constructor
+     */
+    function FlippingCard (container, itemGenerator)
+    {
+        if (! container instanceof Element)
+        {
+            throw new TypeError('contaier must be a DOM Element');
+        }
+        
+        if (
+            ! itemGenerator instanceof Object ||
+            ! itemGenerator.next instanceof Function
+        )
+        {
+            throw new TypeError('itemGenerator must be a generator');
+        }
+        
+        
+        this.setItemGenerator = itemGenerator;
+        this.prepareDom(container);
+        this.setupFresh();
+    }
+    
+    
+    /**
+     * Set content to a DOM Element
+     *
+     * @param {Element} element
+     * @param {string|Element} content - When Element is given, will use
+     *        it's outer HTML, otherwise will be used as a text value
+     */
+    function setElementContent (element, content)
+    {
+        if (! element instanceof Element)
+        {
+            throw new TypeError('element must be a DOM Element');
+        }
+    
+        if (content instanceof Element)
+        {
+            element.innerHTML = content.outerHTML;
+        }
+        else
+        {
+            element.textContent = content;
+        }
+        
+    };
+    
+    
+    /**
+     * Create DOM elements and put them in the container
+     *
+     * Card is hidden by default, {@see appear}.
+     *
+     * @param {Element} container
+     */
+    FlippingCard.prototype.prepareDom = function (container) {
+        
+        if (! container instanceof Element)
+        {
+            throw new TypeError('contaier must be a DOM Element');
+        }
+
+        var questionNav, answerNav;
+        var revealAnswer, gotItWrong, gotItCorrect;
+        
+        this.question = document.createElement('section');
+        this.answer = document.createElement('section');
+        this.cards = document.createElement('dl');
+        this.questionCard = document.createElement('dd');
+        this.answerCard = document.createElement('dt');
+        questionNav = document.createElement('nav');
+        answerNav = document.createElement('nav');
+        revealAnswer = document.createElement('button');
+        gotItWrong = document.createElement('button');
+        gotItCorrect = document.createElement('button');
+        
+        
+        // attributes
+        
+        this.question.classList.add('fcard__card__content');
+        questionNav.classList.add('fcard__card__nav');
+        this.answer.classList.add('fcard__card__content');
+        answerNav.classList.add('fcard__card__nav');
+
+        this.cards.classList.add('fcard__card');
+        this.cards.classList.add('fcard__card');
+
+        this.questionCard.classList.add('fcard__card__question');
+        this.questionCard.setAttribute('tabindex', 1);
+        
+        this.answerCard.classList.add('fcard__card__answer', 'fcard__card__answer--flipped');
+        this.answerCard.setAttribute('tabindex', 1);
+        
+        revealAnswer.setAttribute('type', 'button');
+        gotItWrong.setAttribute('type', 'button');
+        gotItCorrect.setAttribute('type', 'button');
+        
+        revealAnswer.textContent = '↷ show answer';
+        gotItWrong.textContent = '😞 got it wrong';
+        gotItCorrect.textContent = '😃 got it correct';
+        
+        
+        
+        
+        // events
+        
+        revealAnswer.addEventListener('click', this.showAnswer.bind(this));
+        gotItWrong.addEventListener('click', this.wrongAnswer.bind(this));
+        gotItCorrect.addEventListener('click', this.correctAnswer.bind(this));
+
+        this.questionCard.addEventListener('keydown', this.questionKeydownHandler.bind(this));
+        this.answerCard.addEventListener('keydown', this.answerKeydownHandler.bind(this));
+
+        
+        // put together
+        
+        questionNav.appendChild(revealAnswer);
+        answerNav.appendChild(gotItWrong);
+        answerNav.appendChild(gotItCorrect);
+        
+        this.questionCard.appendChild(this.question);
+        this.questionCard.appendChild(questionNav);
+        
+        this.answerCard.appendChild(this.answer);
+        this.answerCard.appendChild(answerNav);
+        
+        this.cards.appendChild(this.questionCard);
+        this.cards.appendChild(this.answerCard);
+        container.appendChild(this.cards);
+    };
+    
+    
+    /**
+     * Setup card using item generator
+     */
+    FlippingCard.prototype.setupFresh = function () {
+        var questionAndAnswer = this.setItemGenerator.next().value;
+        this.setQuestion(questionAndAnswer.question);
+        this.setAnswer(questionAndAnswer.answer);
+    };
+
+    
+    /**
+     * Set question content
+     *
+     * @param {Element|string} question
+     */
+    FlippingCard.prototype.setQuestion = function (question) {
+        setElementContent(this.question, question);
+    };
+    
+    
+    /**
+     * Set answre content
+     *
+     * @param {Element|string} answer
+     */
+    FlippingCard.prototype.setAnswer = function (answer) {
+        setElementContent(this.answer, answer);
+    };
+    
+    
+    /**
+     * Enable or disable all buttons in the card
+     *
+     * @param {boolean} toggle
+     */
+    FlippingCard.prototype.toggleButtonsState = function (toggle) {
+        Array.prototype.forEach.call(
+            this.cards.querySelectorAll('button'),
+            function (button) {
+                button.disabled = ! toggle;
+            }
+        );
+    };
+    
+    
+    /**
+     * Enable all buttons in the card
+     */
+    FlippingCard.prototype.enableButtons = function () {
+        this.toggleButtonsState(true);
+    };
+    
+    
+    /**
+     * Disable all buttons in the card
+     */
+    FlippingCard.prototype.disableButtons = function () {
+        this.toggleButtonsState(false);
+    };
+    
+    
+    /**
+     * Make card disappear
+     */
+    FlippingCard.prototype.disappear = function () {
+        this.cards.classList.remove('fcard__card--onscreen');
+        this.disableButtons();
+    };
+    
+    
+    /**
+     * Make card disappear after wrong answer
+     */
+    FlippingCard.prototype.disappearWrong = function () {
+        this.cards.classList.add('fcard__card--wrong');
+        this.disappear();
+    };
+    
+    
+    /**
+     * Make card disappear after correct answer
+     */
+    FlippingCard.prototype.disappearCorrect = function () {
+        this.cards.classList.add('fcard__card--correct');
+        this.disappear();
+    };
+
+    
+    /**
+     * Make card appear
+     */
+    FlippingCard.prototype.appear = function () {
+        this.showQuestion();
+        this.cards.classList.remove('fcard__card--wrong');
+        this.cards.classList.remove('fcard__card--correct');
+        this.cards.offsetHeight;
+        this.cards.classList.add('fcard__card--onscreen');
+        this.enableButtons();
+    };
+
+    
+    /**
+     * Show question side of the card
+     */
+    FlippingCard.prototype.showQuestion = function () {
+        this.questionCard.classList.remove('fcard__card__question--flipped');
+        this.answerCard.classList.add('fcard__card__answer--flipped');
+        this.questionCard.focus();
+    };
+
+    
+    /**
+     * Show answer side of the card
+     */
+    FlippingCard.prototype.showAnswer = function () {
+        this.questionCard.classList.add('fcard__card__question--flipped');
+        this.answerCard.classList.remove('fcard__card__answer--flipped');
+        this.answerCard.focus();
+    };
+    
+    
+    /**
+     * React to incorrect answer
+     */
+    FlippingCard.prototype.wrongAnswer = function () {
+        this.disappearWrong();
+        this.onWrongAnswer();
+    };
+    
+    
+    /**
+     * React to correct answer
+     */
+    FlippingCard.prototype.correctAnswer = function () {
+        this.disappearCorrect();
+        this.onCorrectAnswer();
+    };
+    
+    
+    /**
+     * Handle keydown event on question card
+     */
+    FlippingCard.prototype.questionKeydownHandler = function (event) {
+        if (40 === event.keyCode)
+        {
+            this.showAnswer();
+            event.preventDefault();
+        }
+    };
+    
+    
+    /**
+     * Handle keydown event on answer card
+     */
+    FlippingCard.prototype.answerKeydownHandler = function (event) {
+        if (37 === event.keyCode)
+        {
+            this.wrongAnswer();
+            event.preventDefault();
+        }
+        if (39 === event.keyCode)
+        {
+            this.correctAnswer();
+            event.preventDefault();
+        }
+    };
+
+    
+    // Export
+    global.FlippingCard = FlippingCard;
+
+}(window, document));
---
+++ index.js
@@ -0,0 +1,288 @@
+(function (global, document, Math) {
+
+    'use strict';
+    
+    var backdrop;
+    
+    /**
+     * Check whether debug is enabled
+     *
+     * @return {boolean}
+     */
+    function debug ()
+    {
+        return -1 !== global.location.hash.indexOf('debug');
+    }
+
+    /**
+     * Check whether browser is cool enough for
+     * enhancements
+     *
+     * @return {boolean}
+     */
+    function isCoolEnough ()
+    {
+        var missingFeatures = [];
+        var element = document.createElement('i');
+        
+        if ('function' !== typeof document.querySelector)
+        {
+            missingFeatures.push('document.querySelector');
+        }
+        
+        if ('function' !== typeof document.querySelectorAll)
+        {
+            missingFeatures.push('document.querySelectorAll');
+        }
+        
+        if ('function' !== typeof Array.prototype.keys)
+        {
+            missingFeatures.push('Array.prototype.keys');
+        }
+        
+        if ('function' !== typeof Array.prototype.forEach)
+        {
+            missingFeatures.push('Array.prototype.forEach');
+        }
+        
+        if ( !global.CSS)
+        {
+            missingFeatures.push('window.CSS');
+        }
+        
+        if ('function' !== typeof global.CSS.supports)
+        {
+            missingFeatures.push('window.CSS.supports');
+        }
+        
+        if (! global.CSS.supports('width', 'calc(1em)'))
+        {
+            missingFeatures.push('CSS: calc');
+        }
+        
+        if (! global.CSS.supports('width', '1vw'))
+        {
+            missingFeatures.push('CSS: vw unit');
+        }
+        
+        if ('function' !== typeof element.addEventListener)
+        {
+            missingFeatures.push('element.addEventListener');
+        }
+        
+        if (undefined === element.classList)
+        {
+            missingFeatures.push('element.classList');
+        }
+        
+        if (! supportsEmoji())
+        {
+            missingFeatures.push('emoji font');
+        }
+
+        if (missingFeatures.length && debug())
+        {
+            alert("Not enhancing, missing features:\n" + missingFeatures.join("\n"));
+        }
+        
+        return 0 === missingFeatures.length;
+    }
+    
+    
+    /**
+     * Detect emoji support by checking whether
+     * two different emojis are rendered the same
+     */
+    function supportsEmoji ()
+    {
+        var canvas, context;
+        var data1, data2;
+        
+        canvas = document.createElement('canvas');
+        
+        if (
+            ! canvas.getContext ||
+            ! canvas.getContext('2d') ||
+            typeof canvas.getContext('2d').fillText !== 'function'
+        )
+        {
+            return false;
+        }
+        else
+        {
+            context = canvas.getContext('2d');
+            context.textBaseline = 'top';
+            context.font = '16px sans-serif';
+            context.fillText('😞', 0, 0);
+            data1 = context.getImageData(0, 0, 16, 16).data;
+            context.fillText('😃', 32, 32);
+            data2 = context.getImageData(32, 32, 16, 16).data;
+            
+            return data1.join('') !== data2.join('');
+        }
+    }
+    
+    
+    /**
+     * Put “Start practice” button on page
+     */
+    function addStartPracticeButton ()
+    {
+        var startPracticeButton;
+        startPracticeButton = document.createElement('button');
+        startPracticeButton.setAttribute('type', 'button');
+        startPracticeButton.classList.add('fcard__startPractise');
+        startPracticeButton.textContent = 'Start practice';
+        
+        startPracticeButton.addEventListener(
+            'click',
+            startPractice
+        );
+        
+        document.querySelector('[data-fcard-place-for-start-practice-button]').appendChild(startPracticeButton);
+    }
+    
+    
+    /**
+     * Create an item from DOM Element
+     *
+     * @param {Element} element - Element with [data-fcard-item]
+     * @return {Object}
+     */
+    function createItemFromElement (element)
+    {
+        var item = {
+            element: element,
+            languageCodes: [],
+            languages: {}
+        };
+        var textElements = element.querySelectorAll('[lang]');
+        var textElement, lang;
+        var idx;
+        for (idx = 0; idx < textElements.length; idx += 1)
+        {
+            textElement = textElements[idx];
+            lang = textElement.lang;
+            if (! item.languages[lang])
+            {
+                item.languageCodes.push(lang);
+                item.languages[lang] = [];
+            }
+            item.languages[lang].push(textElement.textContent);
+        }
+        
+        return item;
+    }
+    
+    
+    /**
+     * Collect all items from the page
+     */
+    function collectItems ()
+    {
+        var elements = document.querySelectorAll('[data-fcard-item]');
+        return Array.prototype.map.call(elements, createItemFromElement);
+    }
+    
+    
+    /**
+     * Get random element from an array
+     *
+     * @param {Array} array
+     * @return Random element
+     */
+    function randomElement (array)
+    {
+        return array[ Math.floor(Math.random() * array.length) ];
+    }
+    
+    function createItemGenerator (items) {
+        return {next: function () {
+            var item = randomElement(items);
+            var lang = randomElement(item.languageCodes);
+            return {value: {
+                question: randomElement(item.languages[lang]),
+                answer: item.element
+            }};
+        }};
+    };
+    
+    
+    /**
+     * Show backdrop
+     */
+    function activateBackdrop ()
+    {
+        var endPracticeButton;
+        
+        backdrop = document.createElement('section');
+        backdrop.classList.add('fcard__card__wrapper');
+        backdrop.addEventListener('keydown', function (event) {
+            if (27 === event.keyCode)
+            {
+                endPractice();
+            }
+        });
+        
+        endPracticeButton = document.createElement('button');
+        endPracticeButton.textContent = '❌';
+        endPracticeButton.classList.add('fcard__endPractice');
+        endPracticeButton.addEventListener('click', endPractice);
+        backdrop.appendChild(endPracticeButton);
+        
+        document.getElementsByTagName('body')[0].appendChild(backdrop);
+    }
+    
+    
+    /**
+     * Start practice session
+     */
+    function startPractice ()
+    {
+        var items = collectItems();
+        var itemGenerator = createItemGenerator(items);
+        var question;
+        var cards = new Array(2);
+        var activeCardIdx;
+        
+        if (undefined === backdrop)
+        {
+            activateBackdrop();
+        }
+        
+        for (activeCardIdx=0; activeCardIdx<cards.length; activeCardIdx += 1)
+        {
+            cards[activeCardIdx] = new FlippingCard(backdrop, itemGenerator);
+            cards[activeCardIdx].onWrongAnswer = cards[activeCardIdx].onCorrectAnswer = function () {
+                var activeCard;
+                var prevCard = cards[activeCardIdx];
+                activeCardIdx = ( activeCardIdx + 1 ) % cards.length;
+                activeCard = cards[ activeCardIdx ];
+                activeCard.setupFresh();
+                activeCard.appear();
+            };
+        }
+        activeCardIdx = 0;
+        cards[activeCardIdx].appear();
+    }
+    
+    
+    /**
+     * End practice
+     */
+    function endPractice ()
+    {
+        if (backdrop instanceof Element)
+        {
+            backdrop.remove();
+            backdrop = undefined;
+        }
+    }
+    
+    
+    if (isCoolEnough())
+    {
+        addStartPracticeButton();
+    }
+
+}(window, document, Math, undefined));
